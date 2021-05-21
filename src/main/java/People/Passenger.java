c9203bd733546a0676ed5e3121a962ed5b177efb
BrockBaron <Nslekkas@gmail.com>
2021-05-21 21:11:07 +0100

    Passenger test defined, methods required.

--- src/main/java/People/Passenger.java
+++ src/main/java/People/Passenger.java
@@ -3,23 +3,16 @@ package People;
 public class Passenger extends Person {
 
     private int noOfBags;
-    private double weightOfBags;
+    private double weightOfEachBag;
     private String flightNo;
     private String seatNo;
 
-    public Passenger(String name, int noOfBags, double weightOfBags, String flightNo, String seatNo) {
+    public Passenger(String name, int noOfBags, double weightOfEachBag, String flightNo, String seatNo) {
         super(name);
         this.noOfBags = noOfBags;
-        this.weightOfBags = weightOfBags;
+        this.weightOfEachBag = weightOfEachBag;
         this.flightNo = flightNo;
         this.seatNo = seatNo;
     }
-
-//    total weight of luggage
-
-//    hand luggage
-
-
-
 }
 
--- src/test/java/PeopleTests/PassengerTest.java
+++ src/test/java/PeopleTests/PassengerTest.java
@@ -4,34 +4,46 @@ import People.Passenger;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
 
     private Passenger passenger;
 
     @Before
     public void before() {
-        passenger = new Passenger("Stan Smith", 2, 18.00, "LM 0397", "8D");
+        passenger = new Passenger("Stan Smith", 2, 12.50, "LM 0397", "8D");
     }
 
     @Test
     public void passengerHasName() {
+        assertEquals("Stan Smith",passenger.getName());
         
     }
 
     @Test
     public void passengerHasNoOfBags() {
+        assertEquals(2, passenger.baggegeCount());
     }
 
     @Test
     public void passengerBagsHaveWeight() {
+        assertEquals(18.00,passenger.baggageWeight());
     }
 
 
     @Test
     public void passengerHasFlightNo() {
+        assertEquals("LM 0397",passenger.getFlightNo());
     }
 
     @Test
     public void passengerHasSeatNo() {
+        assertEquals("8D",passenger.getSeatNo());
+    }
+
+    @Test
+    public void calculatePassengerTotalBaggageWeight() {
+        assertEquals(25.00,passenger.getTotalBaggageWeight());
     }
 }
