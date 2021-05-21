58a1dd312fd731201084a7f9865e0e8ec1ade627
BrockBaron <Nslekkas@gmail.com>
2021-05-21 21:43:33 +0100

    flight class setup start

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,4 +1,23 @@
+import People.CabinCrew;
+import People.FlightCrew;
+import Vehicles.Aircraft;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+
+    private ArrayList<FlightCrew> flightCrew;
+    private ArrayList<CabinCrew> CabinCrew;
+    private Aircraft aircraft;
+    private String flightNo;
+    private String destination;
+    private String departure;
+    private String departureTime;
+
+
+
+
 
 
 //    baggege weight limit per passenger
