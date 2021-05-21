a302db25bde5bd3f61702e6ecfec8e0fea8079a4
BrockBaron <Nslekkas@gmail.com>
2021-05-21 18:10:37 +0100

    CabinCrewTest set up

--- src/test/java/PeopleTests/CabinCrewTest.java
+++ src/test/java/PeopleTests/CabinCrewTest.java
@@ -1,4 +1,24 @@
 package PeopleTests;
 
+import People.CabinCrew;
+import People.Rank;
+import org.junit.Before;
+
 public class CabinCrewTest {
+
+    private CabinCrew purser;
+    private CabinCrew flightAttendant;
+    private String relayCaution;
+
+    @Before
+    public void before(){
+        purser = new CabinCrew("Richard Branson", Rank.PURSER);
+        flightAttendant = new CabinCrew("Shia LeBuff", Rank.ATTENDANT);
+        relayCaution = "Please be careful on the steps as you alight from the aircraft, it has been raining";
+
+
+
+
+
+    }
 }
