21ab2283176d05dc23123c3c6c7a134c88ac41ce
BrockBaron <Nslekkas@gmail.com>
2021-05-21 16:44:02 +0100

    Rank Enum setup

--- src/main/java/People/Rank.java
+++ src/main/java/People/Rank.java
@@ -1,4 +1,18 @@
 package People;
 
 public enum Rank {
+    CAPTAIN("Captain"),
+    FO("First Officer"),
+    PURSER("Purser"),
+    ATTENDANT("Flight Attendant");
+
+    private final String rank;
+
+    Rank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getRank() {
+        return rank;
+    }
 }
