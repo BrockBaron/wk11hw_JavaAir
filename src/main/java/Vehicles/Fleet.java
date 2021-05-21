88b1262e6611bd76b839465d1fe69add9cfe4ac2
BrockBaron <Nslekkas@gmail.com>
2021-05-21 12:30:55 +0100

    Fleet enum setup

--- src/main/java/Vehicles/Fleet.java
+++ src/main/java/Vehicles/Fleet.java
@@ -2,5 +2,20 @@ package Vehicles;
 
 public enum Fleet {
 
+    BRITTENNI("Twin-TurboProp", 2, 8, 54.4),
+    TWINOTTER("Twin-TurboProp", 3, 19, 627.0),
+    EMBREARJ135("Jet",4,37, 1353.0),
+    JETSTREAM41("Jet", 4,29, 957.0 );
 
+    private final String propulsionType;
+    private final int crewCap;
+    private final int paxCap;
+    private final double luggageCap;
+
+    Fleet(String propulsionType, int crewCap, int paxCap, double luggageCap) {
+        this.propulsionType = propulsionType;
+        this.crewCap = crewCap;
+        this.paxCap = paxCap;
+        this.luggageCap = luggageCap;
+    }
 }
